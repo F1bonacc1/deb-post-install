40a6d8972dd6a4606cc1e074b225fb00bff78d7b
Eugene Berger <eugene-b@orbotech.com>
2020-03-20 02:39:05 +0200

    add golang

--- newdeb.sh
+++ newdeb.sh
@@ -17,3 +17,9 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
+
+#golang
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install golang-go
+
