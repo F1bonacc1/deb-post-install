2120ff6618571b9931125f244622d2d04cfac2fd
Eugene Berger <eugene-b@orbotech.com>
2020-03-20 02:01:03 +0200

    First Commit

---
+++ newdeb.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git zsh vim firefox -y
+sudo apt install curl wget -y
+sudo apt install fonts-powerline -y
+
+
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ~ZSH_CUSTOM/plugins/fast-syntax-highlighting
+
+
+#vscode
+sudo apt install software-properties-common apt-transport-https
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
