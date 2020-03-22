0e93adf7bc50a569da735daf434100c4bbbc808f
Eugene Berger <eugene-b@orbotech.com>
2020-03-23 01:02:44 +0200

    added docker, node, zshrc

--- newdeb.sh
+++ newdeb.sh
@@ -1,9 +1,27 @@
+#!/bin/bash
+
+ANSI_RED=$'\033[1;31m'
+ANSI_YEL=$'\033[1;33m'
+ANSI_GRN=$'\033[1;32m'
+ANSI_VIO=$'\033[1;35m'
+ANSI_BLU=$'\033[1;36m'
+ANSI_WHT=$'\033[1;37m'
+ANSI_RST=$'\033[0m'
+
+echo_cmd()    { echo -e "${ANSI_BLU}${@}${ANSI_RST}"; }
+echo_note()   { echo -e "${ANSI_YEL}${@}${ANSI_RST}"; }
+echo_info()   { echo -e "${ANSI_GRN}${@}${ANSI_RST}"; }
+echo_prompt() { echo -e "${ANSI_WHT}${@}${ANSI_RST}"; }
+echo_warn()   { echo -e "${ANSI_YEL}${@}${ANSI_RST}"; }
+echo_debug()  { echo -e "${ANSI_VIO}${@}${ANSI_RST}"; }
+echo_fail()   { echo -e "${ANSI_RED}${@}${ANSI_RST}"; }
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git zsh vim firefox -y
 sudo apt install curl wget -y
 sudo apt install fonts-powerline -y
-
+sudo apt install software-properties-common apt-transport-https ca-certificates -y
 
 #oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -12,17 +30,28 @@ git clone https://github.com/zdharma/fast-syntax-highlighting.git ~ZSH_CUSTOM/pl
 
 
 #vscode
-sudo apt install software-properties-common apt-transport-https
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code
+install_vscode(){
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install code -y
+}
 
 #golang
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update
-sudo apt install golang-go
+install_golang() {
+    sudo add-apt-repository ppa:longsleep/golang-backports
+    sudo apt update
+    sudo apt install golang-go -y
+}
 
+install_docker_ce() {
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
+    sudo apt-get update
+    sudo apt-get install docker-ce -y
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+}
 
 install_dockercompose() {
     if [[ -z $(which docker) ]]; then
@@ -37,5 +66,28 @@ install_typora() {
     wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
     sudo add-apt-repository 'deb https://typora.io/linux ./'
     sudo apt-get update
-    sudo apt-get install typora
+    sudo apt-get install typora -y
 }
+
+install_node() {
+    echo_info " ** Installing NodeJS 12 and npm node package manager ** "
+    cd ;
+    sudo apt install gcc g++ make -y
+    mkdir ~/.npm-global
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+    sudo -v
+    sudo apt update
+    sudo apt install -y nodejs
+    npm config set prefix "$HOME/.npm-global"
+    echo "export PATH=$HOME/.npm-global/bin:$PATH" | tee -a $HOME/.profile
+    source $HOME/.profile
+    npm install npm@latest -g
+}
+
+
+install_vscode
+install_golang
+install_docker_ce
+install_dockercompose
+install_typora
+install_node
